5c9eeec6abc2e848d6807f37d4757f5c85469175
die_narzisse <bulainarcis7@gmail.com>
2022-12-01 20:05:04 +0100

    desktop hover effect updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    
 
+    function listenHoverPicture(){
     $(".project-image").on("mouseover", function(){
         $(this).parent().find('.links').css('opacity', '1');
         $(this).parent().find('img').css('mix-blend-mode', 'overlay');
@@ -18,5 +18,21 @@ $(document).ready(function(){
 
     $(".links").on("mouseleave", function(){
         $(this).parent().find('img').css('mix-blend-mode', 'normal');
+    });
+    }
+
+    listenHoverPicture();
+
+    $(window).on('resize', function(){
+        if($(window).width() < 1200){
+        $(".links").unbind()
+        $(".project-image").unbind();
+        $(".links").css('opacity', '1');
+        $(".project-image").css('mix-blend-mode', 'normal');
+        }
+        else{
+            $(".links").css('opacity', '0');
+            listenHoverPicture();
+        }
     });
 });
